42f27a8b29a46b2e1c771b682b8ed67861a4b0f3
AntoineTR <atheriault@maximiz.com>
2013-05-01 14:09:31 -0400

    PetaPOCO add

---
+++ eq6 Simulation2/eq6 Simulation2/Helpers/EntityWithDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LUJR.Helpers
+{
+
+    public class EntityWithDatabase
+    {
+        protected Database DB;
+
+        public EntityWithDatabase()
+        {
+            DB = new Database("Default");
+        }
+    }
+
+}
---
+++ eq6 Simulation2/eq6 Simulation2/Helpers/PetaPocoDAL.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LUJR.Helpers
+{
+
+    public class PetaPocoDAL<T> where T:new()
+    {
+        #region Memb. Vars
+
+        /// <summary>
+        /// One time DB declaration
+        /// </summary>
+        private Database _DB;
+        public Database DB { get { return _DB; } }
+
+        /// <summary>
+        /// Singleton
+        /// </summary>
+        private static PetaPocoDAL<T> _instance;
+        public static PetaPocoDAL<T> Current { get {
+            if (_instance == null)
+                _instance = new PetaPocoDAL<T>();
+            return _instance;
+        } }
+
+        #endregion
+        #region Constructors
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+	    public PetaPocoDAL()
+	    {
+            _DB = new Database("defaut");
+	    }
+
+        /// <summary>
+        /// Constructor with altername connexion string
+        /// </summary>
+        public PetaPocoDAL(string pConnexionStringName)
+        {
+            _DB = new Database(pConnexionStringName);
+        }
+        
+        #endregion
+        #region Methods
+
+        public T GetOne(int pId) 
+        {
+            return DB.SingleOrDefault<T>(pId);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return DB.Query<T>("");
+        }
+
+        public IEnumerable<T> Get(string pQuery, params object[] pParams)
+        {
+            return DB.Query<T>(pQuery, pParams);
+        }
+
+        public void Insert(T pObj)
+        {
+            DB.Insert(pObj);
+        }
+
+        public void Update(T pObj)
+        {
+            DB.Update(pObj);
+        }
+
+        public void Delete(T pObj)
+        {
+            DB.Delete(pObj);
+        }
+
+        #endregion
+    }
+
+}
